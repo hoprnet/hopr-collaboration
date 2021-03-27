e50f6473a02d928bb0303d03b9a34a664c42b5d4
QYuQianchen <10935300+QYuQianchen@users.noreply.github.com>
2021-03-27 07:57:49 +0100

    Complete on-chain logic. Add deploy script for Kovan

--- on-chain/hardhat.config.ts
+++ on-chain/hardhat.config.ts
@@ -1,6 +1,7 @@
 require('dotenv').config()
 import { HardhatUserConfig, task} from 'hardhat/config'
-
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import chalk from 'chalk'
 import 'hardhat-typechain'
 import '@nomiclabs/hardhat-waffle'
 import '@nomiclabs/hardhat-solhint'
@@ -8,13 +9,17 @@ import '@nomiclabs/hardhat-etherscan'
 import '@nomiclabs/hardhat-solhint'
 import 'solidity-coverage'
 
-const {ETHERSCAN} = process.env
+const {ETHERSCAN, INFURA, DEPLOYER_PRIVATE_KEY} = process.env
 
 const hardhatConfig: HardhatUserConfig = {
   defaultNetwork: 'localhost',
   networks: {
     hardhat: {
     },
+    kovan: {
+      url: `https://kovan.infura.io/v3/${INFURA}`,
+      accounts: [DEPLOYER_PRIVATE_KEY]
+    }
   },
   solidity: {
     compilers: [
@@ -47,5 +52,13 @@ const hardhatConfig: HardhatUserConfig = {
 task('extract', 'Extract ABIs to specified folder', async (...args: any[]) => {
   return (await import('./tasks/extract')).default(args[0], args[1], args[2])
 }).addFlag('target', 'Folder to output contents to')
+
+task('deploy', 'Deploy contract to ethereum')
+  .setAction(async (_taskArgs, hre: HardhatRuntimeEnvironment) => {
+    console.log(`Deploying to ${chalk.hex('#ffffa0').bgHex('#00005f')(` ${hre.network.name} `)}`);
+    const artifact = await hre.ethers.getContractFactory('ChainOnAChip');
+    const contract = await artifact.deploy();
+    console.log(`${chalk.hex('#00005f').bgHex('#ffffa0')(' Greeter ')} deployed to ${contract.address}`);
+});
 
 export default hardhatConfig
